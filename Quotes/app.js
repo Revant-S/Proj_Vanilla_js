bdc78d053faa23ac5765e734ad07c4a4de4b88aa
Revant-S <revant.sinha@gmail.com>
2024-03-02 14:53:16 +0530

    Added the category options

--- Quotes/app.js
+++ Quotes/app.js
@@ -1,6 +1,6 @@
 
-const API_KEY = "YourAPIKEY";
-const api_url ="https://api.api-ninjas.com/v1/quotes?category=happiness";
+const API_KEY = "YOURAPIKEY";
+let api_url ="https://api.api-ninjas.com/v1/quotes?category=";
 
 async function getQuote(url)
 {
@@ -16,19 +16,25 @@ async function getQuote(url)
 }
 const quoteArea = document.querySelector(".quote");
 const quoteAuthor = document.querySelector(".speaker");
-
+const category = document.querySelector("#topics");
 function updateQuote(quoteObject) {
   quoteArea.innerText = quoteObject[0]["quote"];
   quoteAuthor.innerText = quoteObject[0]["author"];
 }
 const btn  = document.querySelector(".btn");
-window.addEventListener("load",async function () {
-  let a = await getQuote(api_url);
-  updateQuote(a);
-}
-)
+// window.addEventListener("load",async function () {
+//   let a = await getQuote(api_url);
+//   updateQuote(a);
+// }
+// )
 
 btn.addEventListener("click" , async function () {
+  // if (!category.value) {
+  //   quoteArea.innerText = "Please select a category!!";
+  //   return;
+  // }
+  api_url =baseurl+ category.value;
   let a = await getQuote(api_url);
+  console.log(a);
   updateQuote(a);
 })
